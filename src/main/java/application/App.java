f3e29410657274fa1066ed710a1d84c058c051c9
tiago1743 <santiago.cardonag@uqvirtual.edu.co>
2025-10-26 18:29:10 -0500

    Proyecto inicial

---
+++ src/main/java/application/App.java
@@ -0,0 +1,19 @@
+package application;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import utils.AppSetup;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
---
+++ src/main/java/service/pago/PagoFactory.java
@@ -0,0 +1,4 @@
+package service.pago;
+
+public class PagoFactory {
+}
